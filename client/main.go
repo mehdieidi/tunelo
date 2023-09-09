1a09b5de7c5aa03501e2423af64363189c7570fb
Mehdi Eidi <mehdiadq@gmail.com>
2023-09-09 10:29:55 +0330

    refactor: add the pkg logger

--- client/main.go
+++ client/main.go
@@ -5,13 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"nhooyr.io/websocket"
+
+	"tunelo/pkg/logger/plain"
 )
 
 func main() {
@@ -19,51 +20,65 @@ func main() {
 	var serverPort string
 	var vpnPort string
 
-	flag.StringVar(&serverIP, "server_ip", "127.0.0.1", "Server IP.")
-	flag.StringVar(&serverPort, "server_port", "23230", "Server port.")
-	flag.StringVar(&vpnPort, "vpn_port", "23233", "VPN port.")
+	flag.StringVar(&serverIP, "server_ip", "127.0.0.1", "Remote proxy-server IP address.")
+	flag.StringVar(&serverPort, "server_port", "23230", "Remote proxy-server port number.")
+	flag.StringVar(&vpnPort, "vpn_port", "23233", "Local VPN port number.")
 	flag.Parse()
+
+	logger := plain.New()
 
 	clientAddr := net.JoinHostPort("127.0.0.1", "23231")
 	clientUDPAddr, err := net.ResolveUDPAddr("udp", clientAddr)
 	if err != nil {
-		log.Fatal("resolving udp addr ", err)
+		logger.Error(fmt.Errorf("resolving udp addr: %v", err), nil)
+		os.Exit(1)
 	}
 
 	clientUDPConn, err := net.ListenUDP("udp", clientUDPAddr)
 	if err != nil {
-		log.Fatal("creating udp listener ", err)
+		logger.Error(fmt.Errorf("creating udp listener: %v", err), nil)
+		os.Exit(1)
 	}
 	defer clientUDPConn.Close()
+
+	logger.Info(fmt.Sprintf("UDP conn: %s", clientAddr), nil)
 
 	vpnAddr := net.JoinHostPort("127.0.0.1", vpnPort)
 	vpnUDPAddr, err := net.ResolveUDPAddr("udp", vpnAddr)
 	if err != nil {
-		log.Fatal("resolving udp addr ", err)
+		logger.Error(fmt.Errorf("resolving vpn udp addr: %v", err), nil)
+		os.Exit(1)
 	}
 
 	localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:")
 	if err != nil {
-		log.Fatal("resolving local UDP addr:", err)
+		logger.Error(fmt.Errorf("resolving local udp addr: %v", err), nil)
+		os.Exit(1)
 	}
 
 	vpnConn, err := net.DialUDP("udp", localAddr, vpnUDPAddr)
 	if err != nil {
-		log.Fatal("creating udp listener ", err)
+		logger.Error(fmt.Errorf("dialling vpn: %v", err), nil)
+		os.Exit(1)
 	}
 	defer vpnConn.Close()
+
+	logger.Info(fmt.Sprintf("VPN UDP conn: %s", vpnAddr), nil)
 
 	serverAddr := net.JoinHostPort(serverIP, serverPort)
 	wsEndpoint := fmt.Sprintf("ws://%s/ws", serverAddr)
 	wsConn, _, err := websocket.Dial(context.Background(), wsEndpoint, nil)
 	if err != nil {
-		log.Fatal("dialling ws ", err)
+		logger.Error(fmt.Errorf("dialling ws: %v", err), nil)
+		os.Exit(1)
 	}
 	defer wsConn.Close(websocket.StatusNormalClosure, "")
 
 	wsNetConn := websocket.NetConn(context.Background(), wsConn, websocket.MessageBinary)
 
-	fmt.Println("ws and udp connected, copying...")
+	logger.Info("ws connected.", nil)
+
+	logger.Info("proxy started...", nil)
 
 	go io.Copy(wsNetConn, clientUDPConn)
 	go io.Copy(vpnConn, wsNetConn)
--- server/main.go
+++ server/main.go
@@ -1,33 +1,38 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"nhooyr.io/websocket"
+
+	"tunelo/pkg/logger"
+	"tunelo/pkg/logger/plain"
 )
 
 type ws struct {
-	udpConn *net.UDPConn
+	vpnConn *net.UDPConn
+	logger  logger.Logger
 }
 
 func (s *ws) handler(w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Accept(w, r, nil)
 	if err != nil {
-		fmt.Println("accepting ws conn", err)
+		s.logger.Error(fmt.Errorf("accepting ws conn: %v", err), nil)
 		return
 	}
 	defer conn.Close(websocket.StatusNormalClosure, "")
 
-	wsNetConn := websocket.NetConn(context.Background(), conn, websocket.MessageBinary)
+	wsNetConn := websocket.NetConn(r.Context(), conn, websocket.MessageBinary)
 
-	go io.Copy(s.udpConn, wsNetConn)
-	io.Copy(wsNetConn, s.udpConn)
+	s.logger.Info("ws connection accepted. Proxy started.", nil)
+
+	go io.Copy(s.vpnConn, wsNetConn)
+	io.Copy(wsNetConn, s.vpnConn)
 }
 
 func main() {
@@ -35,35 +40,41 @@ func main() {
 	var serverPort string
 	var vpnPort string
 
-	flag.StringVar(&serverIP, "server_ip", "127.0.0.1", "Server IP.")
-	flag.StringVar(&serverPort, "server_port", "23230", "Server port.")
-	flag.StringVar(&vpnPort, "vpn_port", "23233", "VPN port.")
+	flag.StringVar(&serverIP, "server_ip", "127.0.0.1", "Proxy server IP address.")
+	flag.StringVar(&serverPort, "server_port", "23230", "Proxy server port number.")
+	flag.StringVar(&vpnPort, "vpn_port", "23233", "Local VPN port number.")
 	flag.Parse()
 
+	logger := plain.New()
+
 	vpnAddr := net.JoinHostPort("127.0.0.1", vpnPort)
-	udpAddr, err := net.ResolveUDPAddr("udp", vpnAddr)
+	vpnUDPAddr, err := net.ResolveUDPAddr("udp", vpnAddr)
 	if err != nil {
-		log.Fatal("resolving udp addr", err)
+		logger.Error(fmt.Errorf("resolving vpn udp addr: %v", err), nil)
+		os.Exit(1)
 	}
 
 	localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:")
 	if err != nil {
-		log.Fatal("resolving local UDP addr:", err)
+		logger.Error(fmt.Errorf("resolving local udp addr: %v", err), nil)
+		os.Exit(1)
 	}
 
-	udpConn, err := net.DialUDP("udp", localAddr, udpAddr)
+	vpnConn, err := net.DialUDP("udp", localAddr, vpnUDPAddr)
 	if err != nil {
-		log.Fatal("dialling vpn", err)
+		logger.Error(fmt.Errorf("dialling vpn: %v", err), nil)
+		os.Exit(1)
 	}
-	defer udpConn.Close()
+	defer vpnConn.Close()
 
-	ws := ws{udpConn: udpConn}
+	ws := ws{vpnConn: vpnConn}
 
 	http.HandleFunc("/ws", ws.handler)
 
 	serverAddr := net.JoinHostPort(serverIP, serverPort)
-	fmt.Println("WebSocket server listening on", serverAddr)
+	logger.Info(fmt.Sprintf("WebSocket server listening on %s", serverAddr), nil)
 	if err := http.ListenAndServe(serverAddr, nil); err != nil {
-		log.Fatal("websocket server listening", err)
+		logger.Error(fmt.Errorf("listen and server: %v", err), nil)
+		os.Exit(1)
 	}
 }
